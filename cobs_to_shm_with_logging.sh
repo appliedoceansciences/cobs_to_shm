ca2bf04aa86905052fd5974c207a4af067836b03
Richard Campbell <richard.campbell@appliedoceansciences.com>
2026-03-19 13:25:50 -0700

    add shm_logger

--- cobs_to_shm_with_logging.sh
+++ cobs_to_shm_with_logging.sh
@@ -14,7 +14,7 @@ printf '%s: data will be written to %s/\n' $(basename "$0") "$DEST" >&2
 # start a child process that ingests data, logs it to disk, and writes each filename to
 # stdout upon completion, whilst also serving the data via shm. the stdout of the child
 # process will become the stdin of this parent script (which replaces itself with xargs)
-exec < <(exec /usr/local/bin/cobs_to_shm "$@")
+exec < <(exec /usr/local/bin/shm_logger "$@")
 
 # catch and ignore sigterm in all remaining processes
 trap '' TERM
