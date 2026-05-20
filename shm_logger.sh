6b4e1994ff6bfcbc82b9f92e492a0e8040136446
Richard Campbell <richard.campbell@appliedoceansciences.com>
2026-05-20 04:28:21 -0700

    write through symlink

--- shm_logger.sh
+++ shm_logger.sh
@@ -2,14 +2,14 @@
 set -e
 
 DESTROOT="$HOME"/element_data
-TIMESTAMP=$(date -u +%Y%m%dT%H%M%SZ)
-DEST="$DESTROOT/$TIMESTAMP"
-
 mkdir -p "$DESTROOT"
-ln -fns "$DEST" "$DESTROOT"/current
+cd "$DESTROOT"
+
+DEST=$(date -u +%Y%m%dT%H%M%SZ)
+ln -fns "$DEST" current
 
 # tell stderr what we are doing and where
-printf '%s: data will be written to %s/\n' $(basename "$0") "$DEST" >&2
+printf '%s: data will be written to %s/\n' $(basename "$0") $(readlink -f current) >&2
 
 # start a child process that ingests data, logs it to disk, and writes each filename to
 # stdout upon completion, whilst also serving the data via shm. the stdout of the child
@@ -23,4 +23,4 @@ trap '' TERM
 # locations in tmpfs, and compress each one and write the compressed output to the final
 # location. this process and the children it spawns will ignore the initial SIGTERM and only
 # exit when they see eof
-exec nice -n20 xargs -I file sh -c 'mkdir -p '"$DEST"' && gzip --fast -c < file > '"$DEST"'/$(basename file).gz; rm file'
+exec nice -n20 xargs -I file sh -c 'mkdir -p $(readlink current) && gzip --fast -c < file > current/$(basename file).gz; rm file'
