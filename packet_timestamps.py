b849fd721430ca819c8b21e639876d7ab8913752
Richard Campbell <richard.campbell@appliedoceansciences.com>
2026-06-25 13:28:12 -0700

    add packet_timestamps.py

---
+++ packet_timestamps.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+# This provides the generator function which knows how to extract sensor-agnostic frames of
+# acoustic sample data from whatever possibly sensor-specific format they are coming from
+from parse_acoustic_packets import yield_acoustic_packets, yield_packet_bytes_from_log_stream
+
+input_source = sys.stdin.buffer
+yield_packet_bytes_function = yield_packet_bytes_from_log_stream
+
+ipacket = 0
+for packet in yield_acoustic_packets(yield_packet_bytes_function, input_source, None):
+    print('%u, %u.%06u, %u.%06u' % (ipacket, packet.timestamp_microseconds / 1000000, packet.timestamp_microseconds % 1000000,
+                           packet.logged_timestamp_microseconds / 1000000, packet.logged_timestamp_microseconds % 1000000))
+    ipacket += 1
